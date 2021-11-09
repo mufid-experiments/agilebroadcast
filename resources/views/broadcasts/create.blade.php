7d8a50d28512b0c26a4e199de0d6000ad7c64f9e
Mufid <mufid@agilecampus.org>
2021-11-09 06:57:03 +0000

    final yay

--- resources/views/broadcasts/create.blade.php
+++ resources/views/broadcasts/create.blade.php
@@ -50,7 +50,7 @@
                                     <label class="form-label col-3 col-form-label">Saluran</label>
                                     <div class="form-selectgroup form-selectgroup-boxes d-flex flex-column col">
                                       <label class="form-selectgroup-item flex-fill">
-                                        <input type="radio" name="channel" value="sms" class="form-selectgroup-input" {{ old('channel') == 'email' ? "checked=checked" : "" }} >
+                                        <input type="radio" name="channel" value="email" class="form-selectgroup-input" {{ old('channel') == 'email' ? "checked=checked" : "" }} >
                                         <div class="form-selectgroup-label d-flex align-items-center p-3">
                                           <div class="me-3">
                                             <span class="form-selectgroup-check"></span>
@@ -59,7 +59,7 @@
                                         </div>
                                       </label>
                                       <label class="form-selectgroup-item flex-fill">
-                                        <input type="radio" name="channel" value="email" class="form-selectgroup-input" {{ old('channel') == 'sms' ? "checked=checked" : "" }} >
+                                        <input type="radio" name="channel" value="sms" class="form-selectgroup-input" {{ old('channel') == 'sms' ? "checked=checked" : "" }} >
                                         <div class="form-selectgroup-label d-flex align-items-center p-3">
                                           <div class="me-3">
                                             <span class="form-selectgroup-check"></span>
